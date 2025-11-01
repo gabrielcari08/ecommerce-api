16d19b2a996eee949a59ee3fb2b68ffba7a2de88
gabrielcari08 <gabrielcari2008@gmail.com>
2025-11-01 16:11:13 -0300

    creacion de las apps necesarias para el proyecto

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'products',
+    'orders',
+    'payments',
+    'carts',
 ]
 
 MIDDLEWARE = [
