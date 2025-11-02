5fac74279fcb390f7a303c9cbc194cea85bb483c
gabrielcari08 <gabrielcari2008@gmail.com>
2025-11-01 23:44:59 -0300

    cambios en products/admin para ver datos en el panel de administracion

--- ecommerce_api/products/admin.py
+++ ecommerce_api/products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from products.models import Product, Category
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name'] #Without 'list_display' in the panel we will see 'Category object (1)'
+    search_fields = ['name'] #With 'search_fields' open the browser. We will can search categories by name
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'is_active', 'created_at'] 
+    list_filter = ['is_active', 'created_at'] #Add filters in the sidebar.
+    search_fields = ['name', 'description']
